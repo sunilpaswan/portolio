95b1c2538517a3a0c0d47e2dd114f1aee871ebeb
sunilpaswan <51480737+sunilpaswan@users.noreply.github.com>
2020-10-18 18:48:01 +0530

    Add files via upload

---
+++ script.js
@@ -0,0 +1,15 @@
+<script type="text/javascript" src="js/JQuery3.3.1.js"></script>
+	<script type="text/javascript">
+	
+	  /*Responsive Navigation*/
+	  $(document).ready(function(){
+	  $('.toggle').click(function(){
+	  $('.toggle').toggleClass('active')
+	  $('nav ul').toggleClass('active-menu')
+		
+		 
+	 })
+ });
+	
+</script>
+  
